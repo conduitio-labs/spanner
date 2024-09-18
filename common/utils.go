d53a2f7249870458f5a1c98711b28b9fc6011e84
Guillem <guillemus@proton.me>
2024-09-18 16:58:10 -0300

    add snapshot iterator to source

--- common/utils.go
+++ common/utils.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"cloud.google.com/go/spanner"
-	sdk "github.com/conduitio/conduit-connector-sdk"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -22,7 +21,6 @@ func NewClient(ctx context.Context, config NewClientConfig) (*spanner.Client, er
 	}
 	if config.Endpoint != "" {
 		options = append(options, option.WithEndpoint(config.Endpoint))
-		sdk.Logger(ctx).Info().Msg("using custom endpoint")
 	}
 
 	return spanner.NewClient(ctx, config.DatabaseName, options...)
