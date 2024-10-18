conduitio-labs/conduit-connector-spanner

add snapshot iterator to source

source_test.go
@@ -5,8 +5,22 @@ import (
 	"testing"
 
 	testutils "github.com/conduitio-labs/conduit-connector-spanner/test"
+	"github.com/conduitio/conduit-commons/config"
+	sdk "github.com/conduitio/conduit-connector-sdk"
 	"github.com/matryer/is"
 )
+
+var singersTable testutils.SingersTable
+
+func testSource(ctx context.Context, is *is.I) (sdk.Source, func()) {
+	source := NewSource()
+	is.NoErr(source.Configure(ctx, config.Config{}))
+	is.NoErr(source.Open(ctx, nil))
+
+	return source, func() {
+		is.NoErr(source.Teardown(ctx))
+	}
+}
 
 func TestTeardownSource_NoOpen(t *testing.T) {
 	is := is.New(t)
@@ -15,4 +29,28 @@ func TestTeardownSource_NoOpen(t *testing.T) {
 	is.NoErr(err)
 }
 
-var singersTable testutils.SingersTable
+func TestSourceSnapshot(t *testing.T) {
+	is := is.New(t)
+	ctx := testutils.TestContext(t)
+
+	testutils.CreateInstance(ctx, is)
+	testutils.SetupDatabase(ctx, is)
+
+	var singers []testutils.Singer
+
+	singer1 := singersTable.Insert(ctx, is, 1, "singer1")
+	singers = append(singers, singer1)
+
+	singer2 := singersTable.Insert(ctx, is, 2, "singer2")
+	singers = append(singers, singer2)
+
+	singer3 := singersTable.Insert(ctx, is, 3, "singer3")
+	singers = append(singers, singer3)
+
+	source, stopSource := testSource(ctx, is)
+	defer stopSource()
+
+	for _, singer := range singers {
+		testutils.ReadAndAssertSnapshot(ctx, is, source, singer)
+	}
+}

conduitio-labs/spanner

source_test.go
@@ -4,12 +4,50 @@ import (
 	"context"
 	"testing"
 
+	testutils "github.com/conduitio-labs/conduit-connector-spanner/test"
+	"github.com/conduitio/conduit-commons/config"
+	sdk "github.com/conduitio/conduit-connector-sdk"
 	"github.com/matryer/is"
 )
+
+func testSource(ctx context.Context, is *is.I) (sdk.Source, func()) {
+	source := NewSource()
+	is.NoErr(source.Configure(ctx, config.Config{}))
+	is.NoErr(source.Open(ctx, nil))
+
+	return source, func() {
+		is.NoErr(source.Teardown(ctx))
+	}
+}
 
 func TestTeardownSource_NoOpen(t *testing.T) {
 	is := is.New(t)
 	con := NewSource()
 	err := con.Teardown(context.Background())
 	is.NoErr(err)
+}
+func TestSourceSnapshot(t *testing.T) {
+	is := is.New(t)
+	ctx := testutils.TestContext(t)
+
+	testutils.CreateInstance(ctx, is)
+	testutils.SetupDatabase(ctx, is)
+
+	var singers []testutils.Singer
+
+	singer1 := testutils.InsertSinger(ctx, is, 1, "singer1")
+	singers = append(singers, singer1)
+
+	singer2 := testutils.InsertSinger(ctx, is, 2, "singer2")
+	singers = append(singers, singer2)
+
+	singer3 := testutils.InsertSinger(ctx, is, 3, "singer3")
+	singers = append(singers, singer3)
+
+	source, stopSource := testSource(ctx, is)
+	defer stopSource()
+
+	for _, singer := range singers {
+		testutils.ReadAndAssertSnapshot(ctx, is, source, singer)
+	}
 }